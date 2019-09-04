c6c52f70969f1154ca679b9093539d52c8c6b31a
zacharycolten <zacharycolten@gmail.com>
2019-09-03 22:36:18 -0500

    New Page

--- src/checkpoint3-stressReliever.js
+++ src/checkpoint3-stressReliever.js
@@ -6,18 +6,24 @@ function countClicks() {
   document.getElementById("clicks").innerHTML = "clicks: " + clicks;
 };
 //timer variable how long the timer is set
-var count = 30;
+var count = 5;
 
 //1000 will  run it every 1 second
 var counter = setInterval(timer, 1000);
 
 //timer function
 function timer() {
-  count = count - 1;
+  count--;
   if (count <= -1) {
     clearInterval(counter);
     return;
   }
+// an attempt to get a popup to occur when the timer hits zero.
+  // function myFunction() {
+  //   if (count = 0) {
+  //     alert("Hello! I am an alert box!");
+  //   }
+  // }
 
   document.getElementById("timer").innerHTML = count + " secs";
 
--- src/dashboard.js
+++ src/dashboard.js
@@ -17,3 +17,34 @@ $(document).ready(function () {
 
 
 })
+
+//variable starting value of clicks
+var clicks = 0;
+//click function and adds number of clicks to p tag with id set to "clicks"
+function countClicks() {
+  clicks += 1;
+  document.getElementById("clicks").innerHTML = "clicks: " + clicks;
+};
+//timer variable how long the timer is set
+var count = 30;
+
+//1000 will  run it every 1 second
+var counter = setInterval(timer, 1000);
+
+//timer function
+function timer() {
+  count--;
+  if (count <= -1) {
+    clearInterval(counter);
+    return;
+  }
+  // an attempt to get a popup to occur when the timer hits zero.
+  // function myFunction() {
+  //   if (count = 0) {
+  //     alert("Hello! I am an alert box!");
+  //   }
+  // }
+
+  document.getElementById("timer").innerHTML = count + " secs";
+
+}
